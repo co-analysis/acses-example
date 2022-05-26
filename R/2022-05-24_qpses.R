e6fd6516072e726a62a80a3824ef1ed35b3b0a67
mattkerlogue <matt.kerlogue@me.com>
2022-05-26 16:50:25 +0100

    create group lookup

--- R/2022-05-24_qpses.R
+++ R/2022-05-24_qpses.R
@@ -34,3 +34,20 @@ write_csv(
   qpses_table,
   file.path("R", "data", "qpses_acses_2021.csv")
 )
+
+group_lookup <- readODS::read_ods(path = ods_file,
+                                  sheet = "Table_11") %>%
+  .[9:192, c(2,6)] %>%
+  set_names(c("organisation", "value")) %>%
+  mutate(organisation = str_squish(str_remove_all(organisation, "\\d"))) %>%
+  drop_na(organisation) %>%
+  mutate(dept_group = if_else(is.na(value), organisation, NA_character_)) %>%
+  fill(dept_group, .direction = "down") %>%
+  drop_na(value) %>%
+  select(dept_group, organisation)
+
+write_csv(
+  group_lookup,
+  file.path("R", "data", "dept_group_lookup.csv")
+)
+)
